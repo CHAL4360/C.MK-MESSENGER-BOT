9aca7d7cb272874aacca7a0c3a7c38751ba5f2cf
BaneleMK12 <114346771+BaneleMK12@users.noreply.github.com>
2026-02-20 14:58:51 +0200

    Update fork.js

--- scripts/cmds/fork.js
+++ scripts/cmds/fork.js
@@ -2,17 +2,17 @@ module.exports = {
   config: {
     name: "fork",
     aliases: ["repo", "source"],
-    version: "1.0",
-    author: "NeoKEX",
+    version: "1.5",
+    author: "CharlesMK",
     countDown: 3,
-    role: 2,
+    role: 0,
     longDescription: "Returns the link to the official, updated fork of the bot's repository.",
     category: "system",
     guide: { en: "{pn}" }
   },
 
   onStart: async function({ message }) {
-    const text = "✓ | Here is the updated fork:\n\nhttps://github.com/NeoKEX/Goatbot-updated.git\n\n" +
+    const text = "✓ | Here is the updated fork:\n\nhttps://github.com/BaneleMK12/C.MK-MESSENGER-BOT.git\n\n" +
                  "Changes:\n1. No Google Credentials needed\n2. Enhanced overall performance\n3. Now using fca-neokex (v1.0.1)\n\n" +
                  "Keep supporting^_^";
     
